d6fb60f57ee319005bf3907802e31b0974d44d67
Yuriy Stul <ysden123@gmail.com>
2018-10-12 16:12:31 +0300

    Spark core

--- src/main/scala/com/stulsoft/monitor/log/analyzer/Application.scala
+++ src/main/scala/com/stulsoft/monitor/log/analyzer/Application.scala
@@ -5,10 +5,17 @@
 package com.stulsoft.monitor.log.analyzer
 
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * @author Yuriy Stul
   */
 object Application extends App with LazyLogging{
 logger.info("==>Application")
+
+  val conf = new SparkConf().setAppName("MonitorLogAnalyzer").setMaster("local[*]")
+  val sc = new SparkContext(conf)
+  
+  sc.stop()
+  logger.info("<==Application")
 }
