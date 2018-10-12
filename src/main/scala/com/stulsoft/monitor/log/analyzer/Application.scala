ec0bd55b172d97b5015eedf992d0c526298ab13d
Yuriy Stul <ysden123@gmail.com>
2018-10-12 15:24:54 +0300

    Initial project

---
+++ src/main/scala/com/stulsoft/monitor/log/analyzer/Application.scala
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul 
+ */
+
+package com.stulsoft.monitor.log.analyzer
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * @author Yuriy Stul
+  */
+object Application extends App with LazyLogging{
+logger.info("==>Application")
+}
